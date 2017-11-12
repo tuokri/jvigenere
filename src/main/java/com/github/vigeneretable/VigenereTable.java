7c11d9103a930a4bbbfac856e6c3cac93b82c5ca
Tuomo Kriikkula <tuomo.kriikkula@student.oulu.fi>
2017-11-12 21:53:10 +0200

    Added (untested) toPlainText implementation.

--- src/main/java/com/github/vigeneretable/VigenereTable.java
+++ src/main/java/com/github/vigeneretable/VigenereTable.java
@@ -37,12 +37,31 @@ public class VigenereTable {
         return newTable;
     }
 
-    public String toCipherText(String plainText) {
+    public String toCipherText(String plainText, String key) {
 
-        return "";
+        if(!StringUtils.containsAll(alphabet, key)) {
+            throw new IllegalArgumentException("Key cannot contain non-alphabet characters");
+        }
+
+        StringBuilder cipher = new StringBuilder();
+        char char_key;
+        int col_key = 0;
+        int row_plain = 0;
+        int k = 0;
+
+        for(char char_plain : plainText.toCharArray()) {
+
+            char_key = key.charAt(k);
+            k = (k + 1) % key.length();
+            col_key = alphabet.indexOf(char_key);
+            row_plain = alphabet.indexOf(char_plain);
+            cipher.append(table.get(row_plain).charAt(col_key));
+        }
+
+        return cipher.toString();
     }
 
-    public String toPlainText(String cipherText) {
+    public String toPlainText(String cipherText, String key) {
 
         return "";
     }
