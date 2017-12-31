abf25af45970316171b18cb5daccf5c02045ae66
Tuomo Kriikula <tuokri@tuta.io>
2017-12-31 10:18:54 +0200

    Proper alignment of MainMenu components. Refactored MainMenu.

--- src/main/java/com/github/client/view/MainMenu.java
+++ src/main/java/com/github/client/view/MainMenu.java
@@ -5,88 +5,165 @@ import java.awt.*;
 
 public class MainMenu {
 
-    private JPanel panel;
+    private JPanel mainPanel;
 
-    private JButton nextButton;
-    private JButton exitButton;
-    private JButton helpButton;
+    private JPanel alphaBetInfoPanel;
+    private JLabel alphabetSelectionText;
+    private JButton alphabetInfoButton;
+
+    private JPanel alphabetButtonPanel;
+    private ButtonGroup alphabetButtonGroup;
     private JButton latinAButton;
     private JButton scandinavianAButton;
     private JButton customAButton;
+
+    private JPanel keyPanel;
     private JButton keyInfoButton;
+    private JLabel keyTextLabel;
+    private JTextField keyInputField;
+
+    private JPanel navigationPanel;
+    private JButton nextButton;
+    private JButton exitButton;
+    private JButton helpButton;
 
     public MainMenu() {
 
-        panel = new JPanel();
-        initComponents(panel);
+        mainPanel = new JPanel();
+        initComponents(mainPanel);
     }
 
     public Container getContentPane() {
 
-        return panel;
+        return mainPanel;
     }
 
+    // TODO: Refactor hard-coded grid constraints.
+    // TODO: Refactor this long-ass abomination of a method.
     private void initComponents(Container pane) {
 
-        JLabel keyLabel;
-        JTextField keyInputField;
-        JButton button;
-
         pane.setLayout(new GridBagLayout());
-        GridBagConstraints constraints = new GridBagConstraints();
 
-        keyLabel = new JLabel("Enter key:");
-        constraints.fill = GridBagConstraints.HORIZONTAL;
-        constraints.gridx = 0;
-        constraints.gridy = 0;
-        pane.add(keyLabel, constraints);
 
+        GridBagConstraints alphabetInfoPanelGbc = new GridBagConstraints();
+        alphabetInfoPanelGbc.gridwidth = 2;
+        alphabetInfoPanelGbc.gridy = 0;
+        alphaBetInfoPanel = new JPanel();
+        alphaBetInfoPanel.setLayout(new GridBagLayout());
+
+        GridBagConstraints alphabetSelectionTextGbc = new GridBagConstraints();
+        alphabetSelectionTextGbc.insets = new Insets(10, 10, 10, 5);
+        alphabetSelectionTextGbc.gridy = 0;
+        alphabetSelectionTextGbc.gridx = 0;
+        alphabetSelectionText = new JLabel("Choose the alphabet");
+        alphaBetInfoPanel.add(alphabetSelectionText, alphabetSelectionTextGbc);
+
+        alphabetSelectionTextGbc.gridx = 1;
+        alphabetSelectionTextGbc.insets = new Insets(0, 0, 0, 0);
+        alphabetInfoButton = createQuestionMarkButton();
+        alphaBetInfoPanel.add(alphabetInfoButton, alphabetSelectionTextGbc);
+
+        pane.add(alphaBetInfoPanel, alphabetInfoPanelGbc);
+
+
+
+
+        GridBagConstraints alphabetButtonPanelGbc = new GridBagConstraints();
+        alphabetButtonPanelGbc.gridy = 1;
+        alphabetButtonPanel = new JPanel();
+        alphabetButtonPanel.setLayout(new GridBagLayout());
+
+        Dimension alphabetButtonSize = new Dimension(100, 30);
+        latinAButton = new JButton("Latin");
+        latinAButton.setPreferredSize(alphabetButtonSize);
+        scandinavianAButton = new JButton("Scandinavian");
+        scandinavianAButton.setPreferredSize(alphabetButtonSize);
+        customAButton = new JButton("Custom...");
+        customAButton.setPreferredSize(alphabetButtonSize);
+
+        alphabetButtonGroup = new ButtonGroup();
+        alphabetButtonGroup.add(latinAButton);
+        alphabetButtonGroup.add(scandinavianAButton);
+        alphabetButtonGroup.add(customAButton);
+
+        GridBagConstraints alphabetButtonConstraints = new GridBagConstraints();
+        alphabetButtonConstraints.ipadx = 35;
+        alphabetButtonConstraints.ipady = 35;
+        alphabetButtonConstraints.weighty = 0.0;
+        alphabetButtonConstraints.weighty = 0.0;
+        alphabetButtonConstraints.fill = GridBagConstraints.NONE;
+        alphabetButtonConstraints.insets = new Insets(5, 5, 5, 5);
+
+        alphabetButtonPanel.add(latinAButton, alphabetButtonConstraints);
+        alphabetButtonPanel.add(scandinavianAButton, alphabetButtonConstraints);
+        alphabetButtonPanel.add(customAButton, alphabetButtonConstraints);
+
+        pane.add(alphabetButtonPanel, alphabetButtonPanelGbc);
+
+
+
+
+        GridBagConstraints keyPanelGbc = new GridBagConstraints();
+        keyPanelGbc.insets = new Insets(10, 10, 10, 10);
+        keyPanelGbc.gridy = 2;
+        keyPanel = new JPanel();
+        pane.add(keyPanel, keyPanelGbc);
+
+        GridBagConstraints keyTextLabelGbc = new GridBagConstraints();
+        keyTextLabelGbc.insets = new Insets(0, 0, 0, 5);
+        keyTextLabel = new JLabel("Enter key:");
+        keyPanel.add(keyTextLabel, keyTextLabelGbc);
+
+        GridBagConstraints keyInputFieldGbc = new GridBagConstraints();
+        keyInputFieldGbc.insets = new Insets(0, 0, 0, 5);
         keyInputField = new JTextField();
-        constraints.fill = GridBagConstraints.HORIZONTAL;
-        constraints.gridx = 1;
-        constraints.gridy = 0;
-        constraints.gridwidth = 3;
-        pane.add(keyInputField, constraints);
-        constraints.gridwidth = 1;
+        keyInputField.setColumns(15);
+        keyPanel.add(keyInputField, keyInputFieldGbc);
+
+        keyInfoButton = createQuestionMarkButton();
+        keyPanel.add(keyInfoButton);
+
+
+
+
+
+
+
+        GridBagConstraints navigationPanelGbc = new GridBagConstraints();
+        navigationPanelGbc.insets = new Insets(5, 5, 5, 5);
+        navigationPanelGbc.gridy = 3;
+        navigationPanelGbc.fill = GridBagConstraints.PAGE_END;
+        navigationPanel = new JPanel();
+        navigationPanel.setLayout(new GridBagLayout());
+
+        exitButton = new JButton("Exit");
+        helpButton = new JButton("Help");
+        nextButton = new JButton("Next");
+
+        GridBagConstraints navigationButtonGbc = new GridBagConstraints();
+
+        navigationButtonGbc.insets = new Insets(5, 5, 5, 75);
+        navigationPanel.add(exitButton, navigationButtonGbc);
+        navigationButtonGbc.insets = new Insets(5, 0, 5, 0);
+        navigationPanel.add(helpButton, navigationButtonGbc);
+        navigationButtonGbc.insets = new Insets(5, 0, 5, 5);
+        navigationPanel.add(nextButton, navigationButtonGbc);
+
+        pane.add(navigationPanel, navigationPanelGbc);
+
+        System.out.println(this + ": initComponents on : " + pane);
+    }
+
+    private JButton createQuestionMarkButton() {
 
         ImageIcon icon;
-        button = new JButton();
+        JButton button = new JButton();
         icon = new ImageIcon(getClass().getResource("/icons/button_question_mark_default_18x18.png"));
         button.setIcon(icon);
         icon = new ImageIcon(getClass().getResource("/icons/button_question_mark_rollover_18x18.png"));
         button.setRolloverIcon(icon);
         icon = new ImageIcon(getClass().getResource("/icons/button_question_mark_pressed_18x18.png"));
         button.setPressedIcon(icon);
-        constraints.fill = GridBagConstraints.HORIZONTAL;
-        constraints.gridx = 5;
-        constraints.gridy = 0;
-        constraints.insets = new Insets(0,10,0,0);
-        pane.add(button, constraints);
-
-        button = new JButton("Exit");
-        constraints.fill = GridBagConstraints.HORIZONTAL;
-        constraints.gridx = 0;
-        constraints.gridy = 1;
-        constraints.insets = new Insets(10,0,0,0);
-        constraints.anchor = GridBagConstraints.PAGE_END;
-        pane.add(button, constraints);
-
-        button = new JButton("Help");
-        constraints.fill = GridBagConstraints.HORIZONTAL;
-        constraints.gridx = 1;
-        constraints.gridy = 1;
-        constraints.insets = new Insets(10,50,0,0);
-        constraints.anchor = GridBagConstraints.PAGE_END;
-        pane.add(button, constraints);
-
-        button = new JButton("Next");
-        constraints.fill = GridBagConstraints.HORIZONTAL;
-        constraints.gridx = 2;
-        constraints.gridy = 1;
-        constraints.insets = new Insets(10,0,0,0);
-        constraints.anchor = GridBagConstraints.PAGE_END;
-        pane.add(button, constraints);
-
-        System.out.println(this + ": initComponents on :" + pane);
+        return button;
     }
 }
