f2776d0a0a0eaa61bd36852fe19807f56efc0642
Tuomo Kriikula <tuokri@tuta.io>
2017-12-31 10:16:54 +0200

    Added annotation and debug print.

--- src/main/java/com/github/client/RunClient.java
+++ src/main/java/com/github/client/RunClient.java
@@ -11,10 +11,12 @@ public class RunClient {
 
         SwingUtilities.invokeLater(new Runnable() {
 
+            @Override
             public void run() {
 
                 Model model = new Model();
                 View view = new View(model, "Vigenècryptor");
+                System.out.println("Initialized client");
             }
         });
     }
